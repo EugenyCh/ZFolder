af6c959d63d7e1a3ce91dc775104194be0eb43e2
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-05-22 12:27:29 +0300

    Time 1

--- DeviceQuaternion4D/DeviceQuaternion4D/kernel.cu
+++ DeviceQuaternion4D/DeviceQuaternion4D/kernel.cu
@@ -248,7 +248,6 @@ int main(int argc, char* argv[])
     // initialize glut
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    //glutInitWindowSize(400, 400);
     glutInitWindowSize(400, 400);
 
     // create window
