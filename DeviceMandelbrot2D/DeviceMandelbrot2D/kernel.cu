672978aeb1b9ab860f80862f57bc15bc629987e1
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-05-22 13:56:53 +0300

    Time 02

--- DeviceMandelbrot2D/DeviceMandelbrot2D/kernel.cu
+++ DeviceMandelbrot2D/DeviceMandelbrot2D/kernel.cu
@@ -247,7 +247,7 @@ int main(int argc, char* argv[])
     // initialize glut
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(960, 640);
+    glutInitWindowSize(1000, 1000);
 
     // create window
     glutCreateWindow("Mandelbrot2D demo");
