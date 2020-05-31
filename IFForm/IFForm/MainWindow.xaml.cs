17acdf1b96b9b523895b24bb6310541e46de64f6
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-05-31 13:20:13 +0300

    .

--- IFForm/IFForm/MainWindow.xaml.cs
+++ IFForm/IFForm/MainWindow.xaml.cs
@@ -20,14 +20,33 @@ namespace IFForm
     /// </summary>
     public partial class MainWindow : Window
     {
+        private enum FractalType
+        {
+            Mand2D = 0,
+            Julia2D = 1,
+            Mand3D = 2,
+            Julia4D = 3
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            ComboType.SelectedIndex = (int)(FractalType.Mand2D);
         }
 
         private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            switch ((FractalType)ComboType.SelectedIndex)
+            {
+                case FractalType.Mand2D:
+                case FractalType.Julia2D:
+                    BlockD.Text = "^ 2";
+                    break;
+                case FractalType.Mand3D:
+                case FractalType.Julia4D:
+                    BlockD.Text = "^ 3";
+                    break;
+            }
         }
     }
 }
