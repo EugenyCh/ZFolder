5562f1f5de14c1ef3b012045fc7a876d36e7f14d
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-06-01 16:14:20 +0300

    .

--- IFForm/IFForm/MainWindow.xaml.cs
+++ IFForm/IFForm/MainWindow.xaml.cs
@@ -28,10 +28,20 @@ namespace IFForm
             Julia4D = 3
         };
 
+        private int WindowSize;
+        private int FractalSize;
+        private int Iterations;
+        private int MaxFractalSize;
+        private bool[] Flags = new bool[4];
+
         public MainWindow()
         {
             InitializeComponent();
             ComboType.SelectedIndex = (int)(FractalType.Mand2D);
+            BoxWinSize.Text = "500";
+            BoxFractalSize.Text = "500";
+            BoxIters.Text = "10";
+            BoxMaxFractalSize.Text = "500";
         }
 
         private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -48,6 +58,74 @@ namespace IFForm
                     break;
             }
             BlockD2.Text = BlockD1.Text;
+        }
+
+        private void BoxWinSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                WindowSize = int.Parse(BoxWinSize.Text);
+                if (WindowSize < 100)
+                    throw new Exception();
+                BoxWinSize.Background = new SolidColorBrush(Colors.White);
+                Flags[0] = true;
+            }
+            catch
+            {
+                BoxWinSize.Background = new SolidColorBrush(Colors.Pink);
+                Flags[0] = false;
+            }
+        }
+
+        private void BoxFractalSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                FractalSize = int.Parse(BoxFractalSize.Text);
+                if (FractalSize < 100)
+                    throw new Exception();
+                BoxFractalSize.Background = new SolidColorBrush(Colors.White);
+                Flags[1] = true;
+            }
+            catch
+            {
+                BoxFractalSize.Background = new SolidColorBrush(Colors.Pink);
+                Flags[1] = false;
+            }
+        }
+
+        private void BoxIters_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                Iterations = int.Parse(BoxIters.Text);
+                if (Iterations < 0)
+                    throw new Exception();
+                BoxIters.Background = new SolidColorBrush(Colors.White);
+                Flags[2] = true;
+            }
+            catch
+            {
+                BoxIters.Background = new SolidColorBrush(Colors.Pink);
+                Flags[2] = false;
+            }
+        }
+
+        private void BoxMaxFractalSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                MaxFractalSize = int.Parse(BoxMaxFractalSize.Text);
+                if (MaxFractalSize < 100)
+                    throw new Exception();
+                BoxMaxFractalSize.Background = new SolidColorBrush(Colors.White);
+                Flags[3] = true;
+            }
+            catch
+            {
+                BoxMaxFractalSize.Background = new SolidColorBrush(Colors.Pink);
+                Flags[3] = false;
+            }
         }
     }
 }
