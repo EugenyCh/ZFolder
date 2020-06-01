47b28945eb0c67034287de72b03e67a6c937756b
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-06-01 17:23:54 +0300

    Form

--- IFForm/IFForm/MainWindow.xaml.cs
+++ IFForm/IFForm/MainWindow.xaml.cs
@@ -38,10 +38,13 @@ namespace IFForm
         private float QuatA;
         private float QuatB;
         private float QuatC;
+        private float JuliaX;
+        private float JuliaY;
 
         private bool[] Flags = new bool[4];
         private bool MandFlag;
         private bool[] QuatFlags = new bool[4];
+        private bool[] JuliaFlags = new bool[2];
 
         public MainWindow()
         {
@@ -52,6 +55,8 @@ namespace IFForm
             BoxFractalSize.Text = "500";
             BoxIters.Text = "10";
             BoxMaxFractalSize.Text = "500";
+            BoxCX.Text = "-0.512511498387847";
+            BoxCY.Text = "0.521295573094847";
             BoxP.Text = "8.0";
             BoxQR.Text = "-0.65";
             BoxQA.Text = "-0.5";
@@ -64,18 +69,26 @@ namespace IFForm
             switch ((FractalType)ComboType.SelectedIndex)
             {
                 case FractalType.Mand2D:
+                    BlockD1.Text = "^ 2";
+                    DockJulia.IsEnabled = false;
+                    DockMand.IsEnabled = false;
+                    DockQuat.IsEnabled = ComboComponent.IsEnabled = false;
+                    break;
                 case FractalType.Julia2D:
                     BlockD1.Text = "^ 2";
+                    DockJulia.IsEnabled = true;
                     DockMand.IsEnabled = false;
                     DockQuat.IsEnabled = ComboComponent.IsEnabled = false;
                     break;
                 case FractalType.Mand3D:
                     BlockD1.Text = "^ 3";
+                    DockJulia.IsEnabled = false;
                     DockMand.IsEnabled = true;
                     DockQuat.IsEnabled = ComboComponent.IsEnabled = false;
                     break;
                 case FractalType.Julia4D:
                     BlockD1.Text = "^ 3";
+                    DockJulia.IsEnabled = false;
                     DockMand.IsEnabled = false;
                     DockQuat.IsEnabled = ComboComponent.IsEnabled = true;
                     break;
@@ -225,6 +238,36 @@ namespace IFForm
             {
                 BoxQC.Background = new SolidColorBrush(Colors.Pink);
                 QuatFlags[3] = false;
+            }
+        }
+
+        private void BoxCX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                JuliaX = float.Parse(BoxCX.Text);
+                BoxCX.Background = new SolidColorBrush(Colors.White);
+                JuliaFlags[0] = true;
+            }
+            catch
+            {
+                BoxCX.Background = new SolidColorBrush(Colors.Pink);
+                JuliaFlags[0] = false;
+            }
+        }
+
+        private void BoxCY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                JuliaY = float.Parse(BoxCY.Text);
+                BoxCY.Background = new SolidColorBrush(Colors.White);
+                JuliaFlags[1] = true;
+            }
+            catch
+            {
+                BoxCY.Background = new SolidColorBrush(Colors.Pink);
+                JuliaFlags[1] = false;
             }
         }
 
